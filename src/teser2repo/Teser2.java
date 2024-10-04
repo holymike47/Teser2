e85778897f82142c00703daa5f4ed3e8c9177157
Mike <holymike47@yahoo.com>
2024-10-04 14:25:03 -0400

    add add()method

--- src/teser2repo/Teser2.java
+++ src/teser2repo/Teser2.java
@@ -2,8 +2,13 @@ package teser2repo;
 
 public class Teser2 {
 
+	static int add(int a, int b) {
+		return a + b;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a, b;
 		int i = 2 + 3;
 		System.out.println(i);
 	}
